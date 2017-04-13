f9bbba80611787b5b34f1856f23cf2b633f7f29c
Samuel Ramos <alu0100815146@ull.edu.es>
2017-04-13 16:04:41 +0100

    Añadiendo firebase y login con google desde firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    constructor() {
+        super();
+        this.state = {
+            user: null
+        };
 
-export default App;
+        this.handleAuth = this.handleAuth.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    componentWillMount () {
+      firebase.auth().onAuthStateChanged(user => {
+        this.setState({ user: user });
+      });
+    }
+
+    handleAuth() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider)
+          .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+          .catch(error => console.log(` Error ${error.code}: ${error.message}`));
+
+    }
+
+    handleLogout() {
+      firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(` Error ${error.code}: ${error.message}`));
+    }
+
+    renderLoginButton() {
+        if (this.state.user) {
+            return (
+                <div>
+                    <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+                    <p> Hola {this.state.user.displayName}! </p>
+                    <button onClick={this.handleLogout}>Salir</button>
+                </div>
+            );
+        } else {
+          return(
+          <button onClick={this.handleAuth}> Login con Google </button>
+         );
+      }
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2> Reactgram </h2>
+                </div>
+                <p className="App-intro"> {this.renderLoginButton()} </p>
+            </div>
+          );
+        }
+    }
+
+    export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
 
+const config = {
+    apiKey: "AIzaSyB_tTzdj5MiBRFrOpOC4yomJ5BM2f_qh5c",
+    authDomain: "reactgram-216ab.firebaseapp.com",
+    databaseURL: "https://reactgram-216ab.firebaseio.com",
+    projectId: "reactgram-216ab",
+    storageBucket: "reactgram-216ab.appspot.com",
+    messagingSenderId: "766232945691"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
